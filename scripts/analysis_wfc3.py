394841eca418cd44d5e1a1581f3dd9d97f4f5f85
Lachlan Marnoch <lachlan.marnoch@students.mq.edu.au>
2018-11-12 22:16:17 +1100

    Further CMD and isochrone analysis

--- scripts/analysis_wfc3.py
+++ scripts/analysis_wfc3.py
@@ -11,10 +11,10 @@ import random as r
 
 def sub_stars(condition):
     subtract = np.zeros(len(B), dtype=bool)
-
+    # Set seed for replicability
+    r.seed(729626)
     for i in range(4):
         for j in range(20):
-            print('Subtracting stars in cell', i, ',', j)
             # This gives the number of stars to remove1 from the corresponding cell in the cluster CMD
             n = sum((B_I_cell == i) & (B_cell == j) & condition)
             # This gives the indices of stars in the corresponding cell in the cluster CMD
@@ -49,6 +49,40 @@ def draw_cells(ax):
         ax.plot([B_I_max, B_I_min], [z, z], c='blue')
 
 
+def draw_sgb_box(ax):
+    ax.plot([left_sgb, left_sgb], [top_sgb, bot_sgb], c='red')
+    ax.plot([left_sgb, right_sgb], [bot_sgb, bot_sgb], c='red')
+    ax.plot([right_sgb, right_sgb], [top_sgb, bot_sgb], c='red')
+    ax.plot([left_sgb, right_sgb], [top_sgb, top_sgb], c='red')
+
+
+def draw_isochrones():
+    plt.plot(B_I_iso_min, B_iso_min, c='purple')
+    plt.plot(B_I_iso_max, B_iso_max, c='violet')
+    plt.plot(B_I_138, B_138, c='blue')
+    plt.plot(B_I_218, B_218, c='red')
+    plt.plot(B_I_best, B_best, c='green')
+
+
+def draw_all_isochrones():
+    for iso in iso_list:
+        plt.plot(iso[:, 29] - iso[:, 34] + B_I_offset, iso[:, 29] + DM)
+
+def get_nearest(xx, arr):
+    return np.abs(xx - arr).argmin()
+
+
+def mse(yy, y_dash):
+    return sum((yy - y_dash) ** 2) / len(yy)
+
+
+# Distance modulus (correction for absolute madnitudes, which are the values we have in the isochrone data, to apparent
+# magnitudes, the values in the DOLPHOT data) to the SMC
+DM = 19.05
+
+B_I_offset = 0.14
+# B_I_offset = 0
+
 # Pixel Scale, from FITS header
 pixel_scale = 0.03962000086903572  # arcsec
 
@@ -63,6 +97,13 @@ B_I_max = 3
 B_I_min = -1
 B_max = 23
 B_min = 17
+
+# Define CMD region of subgiant branch
+
+top_sgb = 21.3
+bot_sgb = 20.25
+left_sgb = 0.8
+right_sgb = 1.25
 
 # Importing data
 print('Importing Data, thank you for your patience')
@@ -89,9 +130,9 @@ print('Number of Stars:', len(data))
 
 # Cut any stars outside of the CMD region of interest (Main Sequence Turnoff and subgiant branch).
 print('Excluding stars outside CMD region of interest')
-#data = data[data[:, 15] - data[:, 28] < B_I_max]
-#data = data[data[:, 15] - data[:, 28] > B_I_min]
-#data = data[data[:, 15] < B_max]
+data = data[data[:, 15] - data[:, 28] < B_I_max]
+data = data[data[:, 15] - data[:, 28] > B_I_min]
+data = data[data[:, 15] < B_max]
 print('Number of Stars:', len(data))
 
 print()
@@ -149,6 +190,10 @@ in_cluster = dist < 50
 in_cluster_equ = np.array(equ_dist < 50. / 3600.)
 print('Stars in cluster:', sum(in_cluster == True))
 
+# Find stars in the SGB region
+in_sgb = (B < top_sgb) & (B > bot_sgb) & (B_I < right_sgb) & (B_I > left_sgb)
+print('SGB: ', sum(in_sgb))
+
 # Decontamination
 # Method from Hu et al
 print('Decontaminating')
@@ -163,6 +208,63 @@ B_cell = np.floor((B - 18.) / 0.25)
 remove1, field1 = sub_stars(condition=y_pix <= 800)
 remove2, field2 = sub_stars(condition=x_pix <= 800)
 remove3, field3 = sub_stars(condition=(80 <= dist) & (dist <= 100))
+remove4, field4 = sub_stars(condition=dist >= 80)
+
+# Import isochrone data
+
+isos = np.genfromtxt(data_dir + "isochrones3.dat")
+# Get the list of ages used in the isochrones
+ages = np.unique(isos[:, 1])
+# Seperate the big bad isochrone file into
+iso_list = []
+for i, a in enumerate(ages):
+    iso = isos[isos[:, 1] == a]
+    iso_list.append(iso)
+
+select_sgb = in_sgb & in_cluster & (remove3 == False)
+
+# Find the points on the isochrone with the nearest x-values to our SGB stars
+mses = np.zeros(len(ages))
+for j, iso in enumerate(iso_list):
+    y_dash = np.zeros(sum(select_sgb))
+    for i, xx in enumerate(B_I[select_sgb]):
+        nearest = get_nearest(xx, iso[:, 29] - iso[:, 34] + B_I_offset)
+        y_dash[i] = iso[nearest, 29] + DM
+    mses[j] = (mse(B[select_sgb], y_dash))
+
+print('Selected stars in SGB:', sum(select_sgb))
+
+# Extract some useful individual isochrones
+
+# Our youngest isochrone
+iso_min = iso_list[0]
+B_iso_min = iso_min[:, 29] + DM
+I_iso_min = iso_min[:, 34] + DM
+B_I_iso_min = B_iso_min - I_iso_min + B_I_offset
+
+# 1.38 Gyrs
+iso_138 = iso_list[np.abs(ages - 1.38e9).argmin()]
+B_138 = iso_138[:, 29] + DM
+I_138 = iso_138[:, 34] + DM
+B_I_138 = B_138 - I_138 + B_I_offset
+
+# 2.18 Gyrs
+iso_218 = iso_list[np.abs(ages - 2.18e9).argmin()]
+B_218 = iso_218[:, 29] + DM
+I_218 = iso_218[:, 34] + DM
+B_I_218 = B_218 - I_218 + B_I_offset
+
+# Our best-fitting isochrone:
+iso_best = iso_list[mses.argmin()]
+B_best = iso_best[:, 29] + DM
+I_best = iso_best[:, 34] + DM
+B_I_best = B_best - I_best + B_I_offset
+
+# Our oldest isochrone:
+iso_max = iso_list[-1]
+B_iso_max = iso_max[:, 29] + DM
+I_iso_max = iso_max[:, 34] + DM
+B_I_iso_max = B_iso_max - I_iso_max + B_I_offset
 
 # PLOTS
 
@@ -272,12 +374,13 @@ plt.show()
 
 fig7, ax7 = plt.subplots(2, 2)
 draw_cells(ax7[0, 0])
-ax7[0, 0].scatter(B_I[y_pix <= 800], B[y_pix <= 800], c='black', marker=',', s=1)
+ax7[0, 0].scatter(B_I[field4], B[field4], c='black', marker=',', s=1)
 ax7[0, 0].set_title('Background CMD, with y <= 800')
 ax7[0, 0].set_xlabel('B - I Colour Index')
 ax7[0, 0].set_ylabel('B magnitude')
 ax7[0, 0].set_xlim(0, 2)
 ax7[0, 0].set_ylim(23, 18)
+draw_sgb_box(ax7[0, 0])
 
 ax7[0, 1].set_title('Cluster CMD')
 draw_cells(ax7[0, 1])
@@ -286,35 +389,39 @@ ax7[0, 1].set_xlabel('B - I Colour Index')
 ax7[0, 1].set_ylabel('B magnitude')
 ax7[0, 1].set_xlim(0, 2)
 ax7[0, 1].set_ylim(23, 18)
+draw_sgb_box(ax7[0, 1])
 
 ax7[1, 0].set_title('Cluster CMD, subtracted stars in red')
 draw_cells(ax7[1, 0])
-ax7[1, 0].scatter(B_I[in_cluster & (remove1 == False)], B[in_cluster & (remove1 == False)], c='black', marker=',', s=1)
-ax7[1, 0].scatter(B_I[remove1], B[remove1], c='red', marker=',', s=1)
+ax7[1, 0].scatter(B_I[in_cluster & (remove4 == False)], B[in_cluster & (remove4 == False)], c='black', marker=',', s=1)
+ax7[1, 0].scatter(B_I[remove4], B[remove4], c='red', marker=',', s=1)
 ax7[1, 0].set_xlabel('B - I Colour Index')
 ax7[1, 0].set_ylabel('B magnitude')
 ax7[1, 0].set_xlim(0, 2)
 ax7[1, 0].set_ylim(23, 18)
+draw_sgb_box(ax7[1, 0])
 
 ax7[1, 1].set_title('Cluster CMD after subtraction')
 draw_cells(ax7[1, 1])
-ax7[1, 1].scatter(B_I[in_cluster & (remove1 == False)], B[in_cluster & (remove1 == False)], c='black', marker=',', s=1)
+ax7[1, 1].scatter(B_I[in_cluster & (remove4 == False)], B[in_cluster & (remove4 == False)], c='black', marker=',', s=1)
 ax7[1, 1].set_xlabel('B - I Colour Index')
 ax7[1, 1].set_ylabel('B magnitude')
 ax7[1, 1].set_xlim(0, 2)
 ax7[1, 1].set_ylim(23, 18)
+draw_sgb_box(ax7[1, 1])
 
 plt.show()
 
 # Recreate Figure 1 in Li et al
 
-fig8, ax8 = plt.subplots(3, 3)
+fig8, ax8 = plt.subplots(3, 4)
 
 ax8[0, 0].scatter(B_I[in_cluster & (remove1 == False)], B[in_cluster & (remove1 == False)], c='black', marker=',', s=1)
 ax8[0, 0].set_xlabel('B - I Colour Index')
 ax8[0, 0].set_ylabel('B magnitude')
 ax8[0, 0].set_xlim(0, 2)
 ax8[0, 0].set_ylim(23, 18)
+draw_sgb_box(ax8[0, 0])
 
 ax8[1, 0].scatter(B_I[field1], B[field1], c='black', marker=',', s=1)
 ax8[1, 0].set_xlabel('B - I Colour Index')
@@ -334,6 +441,7 @@ ax8[0, 1].set_xlabel('B - I Colour Index')
 ax8[0, 1].set_ylabel('B magnitude')
 ax8[0, 1].set_xlim(0, 2)
 ax8[0, 1].set_ylim(23, 18)
+draw_sgb_box(ax8[0, 1])
 
 ax8[1, 1].scatter(B_I[field2], B[field2], c='black', marker=',', s=1)
 ax8[1, 1].set_xlabel('B - I Colour Index')
@@ -353,6 +461,7 @@ ax8[0, 2].set_xlabel('B - I Colour Index')
 ax8[0, 2].set_ylabel('B magnitude')
 ax8[0, 2].set_xlim(0, 2)
 ax8[0, 2].set_ylim(23, 18)
+draw_sgb_box(ax8[0, 2])
 
 ax8[1, 2].scatter(B_I[field3], B[field3], c='black', marker=',', s=1)
 ax8[1, 2].set_xlabel('B - I Colour Index')
@@ -367,4 +476,54 @@ ax8[2, 2].scatter(x[field3], y[field3], c='red', marker=',', s=1)
 ax8[2, 2].set_xlabel('x (arcsec)')
 ax8[2, 2].set_ylabel('y (arcsec)')
 
+ax8[0, 3].scatter(B_I[in_cluster & (remove4 == False)], B[in_cluster & (remove4 == False)], c='black', marker=',', s=1)
+ax8[0, 3].set_xlabel('B - I Colour Index')
+ax8[0, 3].set_ylabel('B magnitude')
+ax8[0, 3].set_xlim(0, 2)
+ax8[0, 3].set_ylim(23, 18)
+draw_sgb_box(ax8[0, 3])
+
+ax8[1, 3].scatter(B_I[field3], B[field3], c='black', marker=',', s=1)
+ax8[1, 3].set_xlabel('B - I Colour Index')
+ax8[1, 3].set_ylabel('B magnitude')
+ax8[1, 3].set_xlim(0, 2)
+ax8[1, 3].set_ylim(23, 18)
+
+ax8[2, 3].axis('equal')
+ax8[2, 3].scatter(x, y, c='black', marker=',', s=1)
+ax8[2, 3].scatter(x[in_cluster], y[in_cluster], c='blue', marker=',', s=1)
+ax8[2, 3].scatter(x[field4], y[field4], c='red', marker=',', s=1)
+ax8[2, 3].set_xlabel('x (arcsec)')
+ax8[2, 3].set_ylabel('y (arcsec)')
+
 plt.show()
+
+# Show isochrones
+
+plt.scatter(B_I[in_cluster & (remove3 == False)], B[in_cluster & (remove3 == False)], c='black', marker=',', s=1)
+plt.scatter(B_I[select_sgb], B[select_sgb], c='violet',
+            marker=',', label='SGB stars')
+draw_isochrones()
+draw_sgb_box(plt)
+plt.xlim(0.1, 1.5)
+plt.ylim(22.5, 19.5)
+plt.legend()
+plt.show()
+
+plt.scatter(B_I[in_cluster & (remove3 == False)], B[in_cluster & (remove3 == False)], c='black', marker=',', s=1)
+plt.scatter(B_I[select_sgb], B[select_sgb], c='violet',
+            marker=',', label='SGB stars')
+draw_all_isochrones()
+draw_sgb_box(plt)
+plt.xlim(0.1, 1.5)
+plt.ylim(22.5, 19.5)
+plt.legend()
+plt.show()
+
+# Plot MSEs of isochrones
+
+plt.plot(ages, mses)
+plt.xlabel('Age')
+plt.ylabel('Mean Sqared Error')
+plt.show()
+print('Optimum age:', ages[mses.argmin()])
